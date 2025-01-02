160aa3002ee71cd1652fa2f9ce59ebd7a1409d4d
Mai Quang <quangmai249@gmail.com>
2025-01-02 23:17:01 +0700

    Added draw path line

--- Assets/Source Code/Level/LevelDesign.cs
+++ Assets/Source Code/Level/LevelDesign.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,6 +12,7 @@ public class LevelDesign : MonoBehaviour
     [SerializeField] Level level = Level.LEVEL_1;
     [SerializeField] TextMeshProUGUI textLevel;
     [SerializeField] bool checking = false;
+    [SerializeField] LineRenderer lineRenderer;
 
     public static LevelDesign Instance;
     private void Awake()
@@ -27,6 +31,46 @@ public class LevelDesign : MonoBehaviour
             this.textLevel.text = level.ToString().Replace('_', ' ');
         else
             this.textLevel.text = PlayerPrefs.GetString("LEVEL").Replace('_', ' ');
+
+        StartCoroutine(nameof(this.DrawPathLine));
+    }
+    IEnumerator DrawPathLine()
+    {
+        List<LineRenderer> ls = new List<LineRenderer>();
+
+        yield return new WaitForSeconds(6f);
+
+        for (int i = 0; i < Directory.GetFiles(FileLocalLink.UserFolderNodePath + this.level.ToString()).Length; i++)
+        {
+            yield return new WaitForSeconds(1f);
+
+            FilePath f = new FilePath(FileLocalLink.UserFolderNodePath + this.level.ToString(), this.level.ToString() + (i + 1));
+            List<Vector3> vec = f.ReadFromFile().ToList();
+
+            LineRenderer lineRenderer_new = Instantiate(lineRenderer);
+            lineRenderer_new.gameObject.transform.SetParent(this.gameObject.transform);
+            lineRenderer_new.positionCount = vec.Count;
+            lineRenderer_new.SetPositions(vec.ToArray());
+            ls.Add(lineRenderer_new);
+        }
+
+        yield return new WaitForSeconds(.5f);
+        this.EnablePathLine(ls, false);
+        yield return new WaitForSeconds(.5f);
+        this.EnablePathLine(ls, true);
+        yield return new WaitForSeconds(.5f);
+        this.EnablePathLine(ls, false);
+        yield return new WaitForSeconds(.5f);
+        this.EnablePathLine(ls, true);
+        yield return new WaitForSeconds(.5f);
+        this.EnablePathLine(ls, false);
+        yield return new WaitForSeconds(.5f);
+        this.EnablePathLine(ls, true);
+    }
+    private void EnablePathLine(List<LineRenderer> ls, bool b)
+    {
+        foreach (LineRenderer lineRenderer in ls)
+            lineRenderer.enabled = b;
     }
     public string GetLevel()
     {
